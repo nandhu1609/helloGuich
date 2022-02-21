eff91b63761b8e9945be83b38fea1e900cd3f94c
numai <numai@Nandha>
2022-02-21 18:50:50 +0530

    3rd commit

---
+++ Git2/src/testGit.java
@@ -0,0 +1,11 @@
+
+public class testGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("2 Hello bois and gurls how are you im gonna be greate as were you");
+		System.out.println("2 This is an project named Git 1 which is an sample proj dont delete this proj");
+
+	}
+
+}
