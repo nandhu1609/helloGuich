e9f31427473e5e81fbff39d594eb86cf05cb5f6e
numai <numai@Nandha>
2022-02-21 18:57:42 +0530

    commit ver 2.2

--- Git2/src/testGit.java
+++ Git2/src/testGit.java
@@ -5,7 +5,7 @@ public class testGit {
 		// TODO Auto-generated method stub
 		System.out.println("2 Hello bois and gurls how are you im gonna be greate as were you");
 		System.out.println("2 This is an project named Git 1 which is an sample proj dont delete this proj");
-
+		System.out.println("Finally succeeded use the token instead of passwrd if cant connect to github");
 	}
 
 }
