6e9d7dcd091926ce84c9cc72e2b379ec2a4f1c41
numai <numai@Nandha>
2022-02-21 18:47:09 +0530

    Second commit

--- Git1/src/Hello.java
+++ Git1/src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello bois and gurls how are you im gonna be greate as were you");
+		System.out.println("This is an project named Git 1 which is an sample proj dont delete this proj");
 
 	}
 
