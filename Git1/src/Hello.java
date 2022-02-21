44518a2fc14d050589703946966245a4823a9e3d
numai <numai@Nandha>
2022-02-21 18:20:20 +0530

    first commit in github

---
+++ Git1/src/Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello bois and gurls how are you im gonna be greate as were you");
+
+	}
+
+}
